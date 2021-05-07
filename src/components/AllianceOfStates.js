34fceca2139cbc3ef943037bae1c49154cd81b57
Jared Drake <jareddrake@Jareds-MacBook-Pro.local>
2021-05-07 13:21:33 -0600

    added mailto links

--- src/components/AllianceOfStates.js
+++ src/components/AllianceOfStates.js
@@ -32,7 +32,11 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Birmingham, AL 35223 </p>
                 <p className='card-text'>Phone: 205-870-9900</p>
                 <p className='card-text'>Fax: 205-870-4407</p>
-                <p className='card-text'>Email: calebc@alabamapolicy.org</p>
+                <p className='card-text'>
+                  <a href='mailto:calebc@alabamapolicy.org'>
+                    Email: calebc@alabamapolicy.org
+                  </a>
+                </p>
                 <p className='card-text'>
                   <a href='https://alabamapolicy.org/'>
                     Website: AlabamaPolicy.org
@@ -51,7 +55,9 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Topeka, KS 66604 </p>
                 <p className='card-text'>Phone: 316-993-3900</p>
                 <p className='card-text'>
-                  Email: kansas@familypolicyalliance.com
+                  <a href='mailto:kansas@familypolicyalliance.com'>
+                    Email: kansas@familypolicyalliance.com
+                  </a>
                 </p>
                 <p className='card-text'>
                   <a href='https://familypolicyalliance.com/?fpastate=KS'>
@@ -73,7 +79,9 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Raleigh, NC 28419</p>
                 <p className='card-text'>Phone: 919-807-0800</p>
                 <p className='card-text'>Fax: 919-807-0900</p>
-                <p className='card-text'>Email: admin@ncfamily.org</p>
+                <p className='card-text'>
+                  <a href='mailto:admin@ncfamily.org'>admin@ncfamily.org</a>
+                </p>
                 <p className='card-text'>
                   <a href='https://www.ncfamily.org/'>Website: ncfamily.org</a>
                 </p>
@@ -91,7 +99,9 @@ const AllianceOfStates = () => {
                 <p className='card-text'>PO Box 231425</p>
                 <p className='card-text'>Anchorage, AK 99523</p>
                 <p className='card-text'>Phone: 903-317-7268</p>
-                <p className='card-text'>Email: jim@akfamily.org</p>
+                <p className='card-text'>
+                  <a href='mailto:jim@akfamily.org'>Email: jim@akfamily.org</a>
+                </p>
                 <p className='card-text'>
                   <a href='https://www.akfamily.org/'>Website: akfamily.org</a>
                 </p>
@@ -108,7 +118,9 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Lexington, KY 40591</p>
                 <p className='card-text'>Phone: 859-255-3330</p>
                 <p className='card-text'>Fax: 859-233-3330</p>
-                <p className='card-text'>Email: tffky@mis.net</p>
+                <p className='card-text'>
+                  <a href='mailto:tffky@mis.net'>Email: tffky@mis.net</a>
+                </p>
                 <p className='card-text'>
                   <a href='https://www.kentuckyfamily.org/'>
                     Website: kentuckyfamily.org
@@ -126,7 +138,9 @@ const AllianceOfStates = () => {
                 <p className='card-text'>3220 18th St. South Ste. 8</p>
                 <p className='card-text'>Fargo, ND 58104</p>
                 <p className='card-text'>Phone: 701-364-0676</p>
-                <p className='card-text'>Email: admin@ndfa.org</p>
+                <p className='card-text'>
+                  <a href='mailto:admin@ndfa.org'>Email: admin@ndfa.org</a>
+                </p>
                 <p className='card-text'>
                   <a href='http://www.ndfa.org/index.php'>Website: ndfa.org</a>
                 </p>
@@ -147,7 +161,11 @@ const AllianceOfStates = () => {
                   Phone: 602-424-2525 or 1-800-FAMILY-1
                 </p>
                 <p className='card-text'>Fax: 602-424-2530</p>
-                <p className='card-text'>Email: info@azpolicy.org</p>
+                <p className='card-text'>
+                  <a href='mailto:info@azpolicy.org'>
+                    Email: info@azpolicy.org
+                  </a>
+                </p>
                 <p className='card-text'>
                   <a href='https://www.azpolicy.org/'>Website: azpolicy.org</a>
                 </p>
@@ -170,7 +188,11 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Phone: 225-344-8533 or:</p>
                 <p className='card-text'>1-800-606-6470</p>
                 <p className='card-text'>Fax: 225-344-9006</p>
-                <p className='card-text'>Email: info@lafamilyforum.org</p>
+                <p className='card-text'>
+                  <a href='info@lafamilyforum.org'>
+                    Email: info@lafamilyforum.org
+                  </a>
+                </p>
                 <p className='card-text'>
                   <a href='https://www.lafamilyforum.org/'>
                     Website: lafamilyforum.org
@@ -189,7 +211,9 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Cincinnati, OH 45241-1997</p>
                 <p className='card-text'>Phone: 513-733-5775</p>
                 <p className='card-text'>Fax: 513-733-5794</p>
-                <p className='card-text'>Email: info@ccv.org</p>
+                <p className='card-text'>
+                  <a href='mailto:info@ccv.org'>Email: info@ccv.org</a>
+                </p>
                 <p className='card-text'>
                   <a href='https://www.ccv.org/'>Website: ndfa.org</a>
                 </p>
@@ -214,7 +238,11 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Little Rock, AR 72201</p>
                 <p className='card-text'>Phone: 501-375-7000</p>
                 <p className='card-text'>Fax: 501-375-7040</p>
-                <p className='card-text'>Email: info@familycouncil.org</p>
+                <p className='card-text'>
+                  <a href='mailto:info@familycouncil.org'>
+                    Email: info@familycouncil.org
+                  </a>
+                </p>
                 <p className='card-text'>
                   <a href='http://familycouncil.org/'>
                     Website: familycouncil.org
@@ -238,7 +266,11 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Augusta, ME 04332-5459</p>
                 <p className='card-text'>Phone: 207-622-7634</p>
                 <p className='card-text'>Fax: 207-621-0035</p>
-                <p className='card-text'>Email: cconley@cclmaine.org</p>
+                <p className='card-text'>
+                  <a href='mailto:cconley@cclmaine.org'>
+                    Email: cconley@cclmaine.org
+                  </a>
+                </p>
                 <p className='card-text'>
                   <a href='https://cclmaine.org/'>Website: cclmaine.org</a>
                 </p>
@@ -255,7 +287,11 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Harrisburg, PA 17101</p>
                 <p className='card-text'>Phone: 717-545-0600</p>
                 <p className='card-text'>Fax: 717-545-8107</p>
-                <p className='card-text'>Email: mail@pafamily.org</p>
+                <p className='card-text'>
+                  <a href='mailto:mail@pafamily.org'>
+                    Email: mail@pafamily.org
+                  </a>
+                </p>
                 <p className='card-text'>
                   <a href='http://www.pafamily.org/'>Website: pafamily.org</a>
                 </p>
@@ -280,7 +316,9 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Phone: 951-742-7100</p>
                 <p className='card-text'>Fax: 951-353-8347</p>
                 <p className='card-text'>
-                  Email: jonathank@californiafamily.org
+                  <a href='mailto:jonathank@californiafamily.org'>
+                    Email: jonathank@californiafamily.org
+                  </a>
                 </p>
                 <p className='card-text'>
                   <a href='http://www.californiafamily.org/'>
@@ -300,7 +338,11 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Woburn, MA 01801</p>
                 <p className='card-text'>Phone: 781-569-0400</p>
                 <p className='card-text'>Fax: 517-374-6112</p>
-                <p className='card-text'>Email: andrew@mafamily.org</p>
+                <p className='card-text'>
+                  <a href='mailto:andrew@mafamily.org'>
+                    Email: andrew@mafamily.org
+                  </a>
+                </p>
                 <p className='card-text'>
                   <a href='http://www.mafamily.org/'>Website: mafamily.org</a>
                 </p>
@@ -317,7 +359,11 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Columbia, SC 29211-1953</p>
                 <p className='card-text'>Phone: 803-733-5600</p>
                 <p className='card-text'>Fax: 866-471-5032</p>
-                <p className='card-text'>Email: email@palmettofamily.org</p>
+                <p className='card-text'>
+                  <a href='mailto:email@palmettofamily.org'>
+                    Email: email@palmettofamily.org
+                  </a>
+                </p>
                 <p className='card-text'>
                   <a href='http://www.palmettofamily.org/'>
                     Website: palmettofamily.org
@@ -337,7 +383,11 @@ const AllianceOfStates = () => {
                 <p className='card-text'>PO Box 558</p>
                 <p className='card-text'>Castle Rock, CO 80104</p>
                 <p className='card-text'>Phone: 877-239-7355</p>
-                <p className='card-text'>Email: info@cofamily.org</p>
+                <p className='card-text'>
+                  <a href='mailto:info@cofamily.org'>
+                    Email: info@cofamily.org
+                  </a>
+                </p>
                 <p className='card-text'>
                   <a href='coloradoaction.org'>Website: coloradoaction.org</a>
                 </p>
@@ -354,7 +404,11 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Lansing, MI 48901-5216</p>
                 <p className='card-text'>Phone: 517-374-1171</p>
                 <p className='card-text'>Fax: 517-374-6112</p>
-                <p className='card-text'>Email: info@michiganfamily.org</p>
+                <p className='card-text'>
+                  <a href='mailto:info@michiganfamily.org'>
+                    Email: info@michiganfamily.org
+                  </a>
+                </p>
                 <p className='card-text'>
                   <a href='http://www.michiganfamily.org/'>
                     Website: michiganfamily.org
@@ -373,7 +427,9 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Rapid City, SD 57709</p>
                 <p className='card-text'>Phone: 605-718-5433</p>
                 <p className='card-text'>
-                  Email: dale@familyheritageallinace.org
+                  <a href='mailto:dale@familyheritageallinace.org'>
+                    Email: dale@familyheritageallinace.org
+                  </a>
                 </p>
                 <p className='card-text'>
                   <a href='http://www.familyheritagealliance.org/'>
@@ -395,7 +451,11 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Hartford, CT 06126</p>
                 <p className='card-text'>Phone: 860-548-0066</p>
                 <p className='card-text'>Fax: 860-548-9545</p>
-                <p className='card-text'>Email: info@ctfamily.org</p>
+                <p className='card-text'>
+                  <a href='mailto:info@ctfamily.org'>
+                    Email: info@ctfamily.org
+                  </a>
+                </p>
                 <p className='card-text'>
                   <a href='http://www.ctfamily.org/'>Website: ctfamily.org</a>
                 </p>
@@ -414,7 +474,9 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Minneapolis, MN 55418-3265</p>
                 <p className='card-text'>Phone: 612-789-8811</p>
                 <p className='card-text'>Fax: 612-789-8858</p>
-                <p className='card-text'>Email: john@mfc.org</p>
+                <p className='card-text'>
+                  <a href='mailto:john@mfc.org'>Email: john@mfc.org</a>
+                </p>
                 <p className='card-text'>
                   <a href='http://www.mfc.org/'>Website: mfc.org</a>
                 </p>
@@ -436,7 +498,9 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Franklin, TN 37064</p>
                 <p className='card-text'>Phone: 615-591-2090</p>
                 <p className='card-text'>Fax: 615-465-8027</p>
-                <p className='card-text'>Email: info@factn.org</p>
+                <p className='card-text'>
+                  <a href='info@factn.org'>Email: info@factn.org</a>
+                </p>
                 <p className='card-text'>
                   <a href='http://www.factn.org/'>Website: factn.org</a>
                 </p>
@@ -458,7 +522,11 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Nicole Theis, President</p>
                 <p className='card-text'>PO Box 925</p>
                 <p className='card-text'>Seafor, DE 19973</p>
-                <p className='card-text'>Email: info@delawarefamilies.org</p>
+                <p className='card-text'>
+                  <a href='mailto:info@delawarefamilies.org'>
+                    Email: info@delawarefamilies.org
+                  </a>
+                </p>
                 <p className='card-text'>
                   <a href='http://www.delawarefamilies.org/'>
                     Website: delawarefamilies.org
@@ -476,7 +544,11 @@ const AllianceOfStates = () => {
                 <p className='card-text'>1430 Triad Center Fr. Ste. B</p>
                 <p className='card-text'>St. Peters, MO 63376</p>
                 <p className='card-text'>Phone: 636-922-7771</p>
-                <p className='card-text'>Email: info-plus@missourifamily.org</p>
+                <p className='card-text'>
+                  <a href='mailto:info-plus@missourifamily.org'>
+                    Email: info-plus@missourifamily.org
+                  </a>
+                </p>
                 <p className='card-text'>
                   <a href='http://www.missourifamily.org/'>
                     Website: missourifamily.org
@@ -494,7 +566,11 @@ const AllianceOfStates = () => {
                 <p className='card-text'>900 Congress Ste. 220</p>
                 <p className='card-text'>Austin, TX 78701</p>
                 <p className='card-text'>Phone: 512-478-2220</p>
-                <p className='card-text'>Email: info@txvalues.org</p>
+                <p className='card-text'>
+                  <a href='mailto:info@txvalues.org'>
+                    Email: info@txvalues.org
+                  </a>
+                </p>
                 <p className='card-text'>
                   <a href='http://www.txvalues.org/'>Website: txvalues.org</a>
                 </p>
@@ -520,7 +596,11 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Orlando, FL 32806</p>
                 <p className='card-text'>Phone: 407-251-5130</p>
                 <p className='card-text'>Fax: 407-251-0023</p>
-                <p className='card-text'>Email: info@flfamily.org</p>
+                <p className='card-text'>
+                  <a href='mailto:info@flfamily.org'>
+                    Email: info@flfamily.org
+                  </a>
+                </p>
                 <p className='card-text'>
                   <a href='http://www.floridafamilyaction.org/'>
                     Website: floridafamilyaction.org
@@ -539,7 +619,11 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Laurel, MT 59044</p>
                 <p className='card-text'>Phone: 406-628-1141</p>
                 <p className='card-text'>Fax: 406-628-1171</p>
-                <p className='card-text'>Email: jeff@montanafamily.org</p>
+                <p className='card-text'>
+                  <a href='mailto:jeff@montanafamily.org'>
+                    Email: jeff@montanafamily.org
+                  </a>
+                </p>
                 <p className='card-text'>
                   <a href='http://www.montanafamily.org/'>
                     Website: montanafamily.org
@@ -562,7 +646,9 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Phone: 804-343-0010</p>
                 <p className='card-text'>Fax: 804-343-0050</p>
                 <p className='card-text'>
-                  Email: vafamily@familyfoundation.org
+                  <a href='mailto:vafamily@familyfoundation.org'>
+                    Email: vafamily@familyfoundation.org
+                  </a>
                 </p>
                 <p className='card-text'>
                   <a href='http://www.familyfoundation.org/'>
@@ -589,7 +675,11 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Kaneohe, HI 96744-5224</p>
                 <p className='card-text'>Phone: 808-203-6704</p>
                 <p className='card-text'>Fax: 808-261-7022</p>
-                <p className='card-text'>Email: info@hawaiifamilyforum.org</p>
+                <p className='card-text'>
+                  <a href='mailto:info@hawaiifamilyforum.org'>
+                    Email: info@hawaiifamilyforum.org
+                  </a>
+                </p>
                 <p className='card-text'>
                   <a href='http://www.hawaiifamilyforum.org/'>
                     Website: hawaiifamilyforum.org
@@ -609,7 +699,9 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Phone: 402-477-3191</p>
                 <p className='card-text'>Phone: 1-888-777-5188</p>
                 <p className='card-text'>
-                  Email: al@nebraskafamilyalliance.org
+                  <a href='mailto:al@nebraskafamilyalliance.org'>
+                    Email: al@nebraskafamilyalliance.org
+                  </a>
                 </p>
                 <p className='card-text'>
                   <a href='http://nebraskafamilyalliance.org/'>
@@ -631,7 +723,11 @@ const AllianceOfStates = () => {
                 <p className='card-text'>Lynnwood, WA 98087</p>
                 <p className='card-text'>Phone: 425-608-0242</p>
                 <p className='card-text'>Fax: 425-608-7216</p>
-                <p className='card-text'>Email: jbackholm@fpiw.org</p>
+                <p className='card-text'>
+                  <a href='mailto:jbackholm@fpiw.org'>
+                    Email: jbackholm@fpiw.org
+                  </a>
+                </p>
                 <p className='card-text'>
                   <a href='http://www.fpiw.org/'>Website: fpiw.org</a>
                 </p>
@@ -654,7 +750,11 @@ const AllianceOfStates = () => {
                 <p className='card-text'>PO Box 563</p>
                 <p className='card-text'>Eagle, ID 83616</p>
                 <p className='card-text'>Phone: 208-939-6622</p>
-                <p className='card-text'>Email: info@cfcidaho.org</p>
+                <p className='card-text'>
+                  <a href='mailto:info@cfcidaho.org'>
+                    Email: info@cfcidaho.org
+                  </a>
+                </p>
                 <p className='card-text'>
                   <a href='http://www.cfcidaho.org/'>Website: cfcidaho.org</a>
                 </p>
